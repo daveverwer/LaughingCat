196de1bdbd1a8aade8eec42fe231a873309ea8c5
maryrosedevega <143856348+maryrosedevega@users.noreply.github.com>
2023-12-04 21:12:21 +0800

    Delete Finished App directory

--- Finished App/LaughingCat/LaughingCatViewController.h
+++
@@ -1,8 +0,0 @@
-@interface LaughingCatViewController : UIViewController
-
-@property (nonatomic, retain) IBOutlet UIButton *catHeadButton;
-@property (nonatomic, retain) AVAudioPlayer *audioPlayer;
-
-- (IBAction)catHeadButtonTapped:(id)sender;
-
-@end
